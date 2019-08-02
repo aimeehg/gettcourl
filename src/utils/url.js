71c0f9c893607543d8c5dd42b56ff32a31ced1a9
Aimee Hernandez <hernandez.aimee@outlook.com>
2019-08-02 18:10:56 -0500

    quoted tweets allowed

--- src/utils/url.js
+++ src/utils/url.js
@@ -4,7 +4,7 @@ const T = require('./config')
 
     T.get('statuses/show/:id', { id }, function(err, data, response) {
        try {
-        callback(undefined, data.entities.media[0].url)
+        callback(undefined, data.entities.urls[0].url)
        } catch (error) {
         callback('An error occurred while getting the data', undefined)
        }
