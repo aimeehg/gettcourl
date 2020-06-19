1ea5771140eaf16ad35e8e6abd32fc467dd782e9
Aimee Hernandez <hernandez.aimee@outlook.com>
2020-06-19 15:33:49 -0500

    pruebas3

--- src/utils/url.js
+++ src/utils/url.js
@@ -5,6 +5,7 @@ const T = require('./config')
     T.get('statuses/show/:id', { id }, function(err, data, response) {
       console.log("entro?3")
       console.log(response)
+      console.log(data)
         try {
          data.is_quote_status ?
           callback(undefined, data.entities.urls[0].url) : callback(undefined, data.entities.media[0].url)
