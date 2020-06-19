274fc5a5a29d4af05d321d256fa26eeb8f4dac05
Aimee Hernandez <hernandez.aimee@outlook.com>
2020-06-19 17:01:23 -0500

    pruebas5

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const T = new Twit({
     consumer_key:         process.env.TWITTER_CONSUMER_KEY,
     consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key:         process.env.ACCESS_TOKEN_KEY,
+    access_token:     process.env.ACCESS_TOKEN_KEY,
     access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
